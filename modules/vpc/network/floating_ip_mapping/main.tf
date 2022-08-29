0d1c4d7b068932a78c9fc9577eda689d30b34a0c
Andrey Oleynik <a.y.oleynik@gmail.com>
2022-08-29 12:31:03 +0300

    fix port id in floating ip mapping module

--- modules/vpc/network/floating_ip_mapping/main.tf
+++ modules/vpc/network/floating_ip_mapping/main.tf
@@ -4,6 +4,6 @@ module "floating_ip" {
 }
 
 resource "openstack_networking_floatingip_associate_v2" "association" {
-  port_id     = var.network_port_id
+  port_id     = var.port_id
   floating_ip = module.floating_ip.floating_ip_address
 }
