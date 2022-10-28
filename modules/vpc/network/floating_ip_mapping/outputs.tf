e238bcf7f0def9e4a1ef57c1ecd86c8a097ef08e
Andrey Oleynik <a.y.oleynik@gmail.com>
2022-10-29 01:45:26 +0300

    change floating ip output

--- modules/vpc/network/floating_ip_mapping/outputs.tf
+++ modules/vpc/network/floating_ip_mapping/outputs.tf
@@ -10,14 +10,6 @@ output "id" {
   value = data.openstack_networking_floatingip_v2.floating_ip.id
 }
 
-output "pool" {
-  value = data.openstack_networking_floatingip_v2.floating_ip.pool
-}
-
 output "port_id" {
   value = var.port_id
-}
-
-output "region" {
-  value = data.openstack_networking_floatingip_v2.floating_ip.region
 }
