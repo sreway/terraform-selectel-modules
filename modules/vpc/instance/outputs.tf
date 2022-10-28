02007d1c0a312c01626de18f1e6345aee1bb6a3e
Andrey Oleynik <a.y.oleynik@gmail.com>
2022-10-29 00:41:26 +0300

    change output instances

--- modules/vpc/instance/outputs.tf
+++ modules/vpc/instance/outputs.tf
@@ -6,7 +6,7 @@ output "port_id" {
   value = openstack_networking_port_v2.network_port.id
 }
 
-output "ip_v4" {
+output "access_ip_v4" {
   value = openstack_compute_instance_v2.compute_instance.access_ip_v4
 }
 
