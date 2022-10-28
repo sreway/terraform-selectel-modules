85d4694ba6d0ae56844cf7e07039ef0421a349b9
Andrey Oleynik <a.y.oleynik@gmail.com>
2022-10-29 00:50:47 +0300

    change ansible inventory input variables

--- modules/vpc/ansible_inventory/vars.tf
+++ modules/vpc/ansible_inventory/vars.tf
@@ -1,16 +1,18 @@
-variable "instance_id" {
+variable "instances" {
   type = map(object({
     id = string
+    access_ip_v4 = string
+    tags = set(string)
   }))
 
-  description = "Compute instance ids"
+  description = "Compute instance data (datasource openstack_compute_instance_v2)"
 }
 
-
-variable "floating_ip" {
+variable "floating_ips" {
   type = map(object({
-    ip_address = string
+    address = string
+    fixed_ip = string
   }))
 
-  description = "Available floating ip address"
+  description = "Floating ip address data (datasource openstack_networking_floatingip_v2)"
 }
